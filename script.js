f6addf155e6a434175ba10b0ca5c67e4a69a17ef
Aishah Ahmeed <aishahahmeed@gmail.com>
2023-06-29 20:04:43 -0600

    Function to push new book to library

---
+++ script.js
@@ -0,0 +1,23 @@
+let myLibrary = [];
+
+function Book(title, author, pages) {
+    // constructor function to create book
+    this.title = title
+    this.author = author
+    this.pages = pages
+}
+
+let book1 = new Book('book1', 'author1', 100);
+let book2 = new Book('book2', 'author2', 200)
+let book3 = new Book('book3', 'author3', 300)
+
+
+let inputTitle = 'Novel'
+let inputAuthor = 'Sam'
+let inputPages = 20;
+
+function addBookToLibrary() {
+    let newBook = new Book(inputTitle, inputAuthor, inputPages);
+    myLibrary.push(newBook);
+}
+
