3da0016d4833423ddc712a3a733da16c9c6a78ea
Aishah <aishahahmeed@gmail.com>
2025-12-17 23:02:47 +0100

    Initialise variables, func to add new books

--- script.js
+++ script.js
@@ -1,4 +1,40 @@
-const container = document.querySelector('#container')
+// main library
+const container = document.querySelector('#books');
+const addNew = document.querySelector('#new_book');
+
+// form
+const form = document.querySelector('#form');
+const submit = document.querySelector('#confirm');
+const cancel = document.querySelector('#cancel');
+const dialog = document.querySelector('.dialog');
+const checkbox = document.querySelector('#read');
+
+
+// dialog actions
+addNew.addEventListener('click', () => {
+  dialog.showModal();
+});
+
+cancel.addEventListener('click', () => {
+  dialog.close();
+});
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const title = event.target.title.value;
+  const author = event.target.author.value;
+  const pages = event.target.pages.value;
+  let read;
+
+  if (checkbox.checked) {
+    read = 'Yes';
+  } else {
+    read = 'No'
+  }
+  addBookToLibrary(title, author, pages, read);
+  event.target.reset();
+  dialog.close();
+})
 
 // All of your book objects are going to be stored in an array, so you’ll need a constructor for books. 
 
@@ -8,7 +44,7 @@ function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.pages = read;
+  this.read = read;
   this.id = crypto.randomUUID()
 }
 
@@ -17,6 +53,7 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(title, author, pages, read) {
   let newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
+  showBook(newBook);
 }
 
 // Also, all of your book objects should have a unique id, which can be generated using crypto.randomUUID(). This ensures each book has a unique and stable identifier, preventing issues when books are removed or rearranged. Your code should look something like this (we’re showing only a basic skeleton without function parameters):
@@ -24,18 +61,28 @@ function addBookToLibrary(title, author, pages, read) {
 
 // Write a function that loops through the array and displays each book on the page. You can display them in some sort of table, or each on their own “card”. It might help for now to manually add a few books to your array so you can see the display.
 
+// myLibrary.forEach(showBook);
 
-addBookToLibrary('Yellowface', 'R.F. Kuang', 285, true);
-addBookToLibrary('Maybe You Should Talk to Someone', 'Lori Gottlieb', 359, true);
-addBookToLibrary('1984', 'George Orwell', 328, false);
-
-container.style.backgroundColor = 'pink';
-container.style.height = '300px'
-
-myLibrary.forEach(showBook) 
+addBookToLibrary('Yellowface', 'R.F. Kuang', 285, 'Yes');
+addBookToLibrary('Maybe You Should Talk to Someone', 'Lori Gottlieb', 359, 'Yes');
+addBookToLibrary('1984', 'George Orwell', 328, 'No');
 
 function showBook(book) {
     const newBook = document.createElement('div');
-    newBook.innerText = book.title;
+    newBook.setAttribute('class', 'book');
+
+    const title = document.createElement('div');
+    title.textContent = `${book.title}`;
+    const author = document.createElement('div');
+    author.textContent = `Author: ${book.author}`;
+    const pages = document.createElement('div');
+    pages.textContent - `${book.pages} pages`;
+    const read = document.createElement('div');
+    read.textContent = `Read: ${book.read}`;
+
+    newBook.append(title);
+    newBook.append(author);
+    newBook.append(pages);
+    newBook.append(read);
     container.append(newBook);
 }
